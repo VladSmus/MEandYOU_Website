6e129d08e5f694fdd49f5aca6ea1b1c4a37d0df5
VladSmus <vladsmusev2004@yandex.ru>
2026-02-11 22:08:12 +0300

    photo upload function

--- components/PhotoUpload.tsx
+++ components/PhotoUpload.tsx
@@ -1,12 +1,54 @@
 "use client";
 
+import { uploadProfilePhoto } from "@/lib/actions/profile";
+import { useRef, useState} from "react";
 
 
 export default function PhotoUpload({
-  onPhotoUpload,
+  onPhotoUploaded,
 }: {
-  onPhotoUpload: (url: string) => void;
+  onPhotoUploaded: (url: string) => void;
 }) {
+    const [uploading, setUploading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    async function handelFileSelect(event: React.ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?[0];
+        if (!file) return;
+        
+        if (!file.type.stsartWith("image/")) {
+            setError("Please select an image file");
+            return;
+        }
+        if (file.size > 5 * 1024 * 1024) {
+        setError("File size must be less than 5 MB");
+        return;
+    }
+
+    setUploading(true);
+    setError(null);
+
+
+    try {
+        const result = await uploadProfilePhoto(file);
+        if (result.success && result.url) {
+            onPhotoUploaded(result.url);
+            setError(null);
+        } else {
+            setError(result.error ?? "Failed to upload photo.");
+        }
+
+    } catch (error) {
+        setError("Failed to change photo.");
+    } finally {
+        setUploading(false);
+    }
+    }
+
+    
+
+
   return (
     <div className="absolute bottom-0 right-0">
       <input type="file" accept="image/*" className="hidden" />
