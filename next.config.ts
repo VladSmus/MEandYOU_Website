47c862b45b7c437519f25dbcbd8e6ec561aac7f2
VladSmus <vladsmusev2004@yandex.ru>
2026-02-12 20:34:58 +0300

    next.config settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,59 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "bvnztdunkjtbbzjzlxrm.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-77.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-15.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-74.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-2.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-63.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-12.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-67.userapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-30.vkuserphoto.ru",
+      },
+      {
+        protocol: "https",
+        hostname: "sun9-64.vkuserphoto.ru",
+      },
+    ],
+  },
+  // reactCompiler: true,
 };
 
 export default nextConfig;
