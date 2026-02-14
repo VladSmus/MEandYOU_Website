58e0b1d6e5349b935dccb8b7fb4f09e0a0f43f85
VladSmus <vladsmusev2004@yandex.ru>
2026-02-14 10:50:12 +0300

    page interface for chat

--- components/StreamChatInterface.tsx
+++ components/StreamChatInterface.tsx
@@ -1,2 +1,40 @@
 import { UserProfile } from "@/app/profile/page";
-import { createOrGetChannel, create}
+import { createOrGetChannel, createVideoCall, getStreamUserToken } from "@/lib/actions/stream";
+import { useRouter } from "next/navigation";
+import { RefObject, useEffect, useImperativeHandle, useRef, useState } from "react";
+import { Channel, Event, StreamChat } from "stream-chat-react";
+import { text} from "stream/consumers";
+import VideoCall from "./VideoCall";
+
+interface Message {
+    id: string;
+    text: string;
+    sender: "me" | "other";
+    timestamp: Date;
+    user_id: string;
+}
+
+export default function StreamChatInterface({
+    otherUser,
+    ref,
+
+}: {
+    otherUser: UserProfile;
+    ref: RefObject<{handleVideoCall : () => void} | null>;
+}) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [currentUserId, setCurrentUserId] = useState<string>("");
+    const [message, setMessage] = useState<Message[]>([]);
+    const [newMessage, setNewMessage] = useState<string>("");
+    const [isTyping, setIsTyping] = useState<boolean>(false);
+
+    const [client, setClient] = useState<StreamChat | null>(null);
+    const [channel, setChannel] = useState<Channel | null>(null);
+
+    const [showScrollButton, setShowScrollButton] = useState<boolean>(false);
+
+    const [vidoeCallId, setVideoCallId] = useState<string>("");
+    const [showVideoCall, setShowVideoCall] = useState<string>("");
+    const 
+}
